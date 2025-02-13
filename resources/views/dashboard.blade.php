c7f3c1e5e8b03a564cf5cf7defd40d77f3d7406f
tav0ks <viniciustavolaro@gmail.com>
2025-02-13 18:49:59 -0300

    adiciona seção para conectar conta Steam no dashboard e incorpora um iframe de instantâneo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,33 @@
             </div>
         </div>
     </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col items-center gap-3">
+                    {{ __('Connect your steam account!') }}
+                    <a href="{{ route('profile.edit') }}"
+                        class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">Connect</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col items-center gap-3">
+                    <iframe width="110" height="200"
+                        src="https://www.myinstants.com/instant/me-mata-de-uma-vez-31687/embed/" frameborder="0"
+                        scrolling="no"></iframe>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
 </x-app-layout>
